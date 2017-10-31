c2509d18be0ab96faaf5a47b70f75af10cb197ed
Tsesar Rizqi <tsesarrizqi@gmail.com>
2017-10-31 11:20:18 +0700

    transfer cli

--- sisdis1/views.py
+++ sisdis1/views.py
@@ -293,3 +293,41 @@ def transfer(req):
 		resp = {}
 		resp['status_transfer'] = -99
 		return JsonResponse(resp)
+
+def transfer_ke(req):
+	body_unicode = req.body.decode('utf-8')
+	body = json.loads(body_unicode)
+	user_id = body['user_id']
+	ip_tujuan = body['ip_tujuan']
+	jumlah_transfer = body['jumlah_transfer']
+
+	nasabah = Nasabah.objects.filter(user_id = user_id)
+	if len(nasabah) == 0:
+		print('User ID belum terdaftar di sistem ini.')
+		return
+
+	body_post_saldo = {'user_id':user_id}
+	resp_saldo = requests.post('http://'+ip_tujuan+'/ewallet/getSaldo', json = body_post)
+	body_saldo_unicode = resp_saldo.text
+	body_saldo = json.loads(body_saldo_unicode)
+	if str(body_saldo['nilai_saldo']) == '-1':
+		body_post_register = {'user_id':user_id, 'nama':nasabah[0].nama}
+		resp_register = requests.post('http://'+ip_tujuan+'/ewallet/resgiter', json = body_post_register)
+		body_register_unicode = resp_register.text
+		body_register = json.loads(body_register_unicode)
+		if str(body_register['status_register']) != '1':
+			print('Terjadi kesalahan. Transfer sejumlah ',jumlah_transfer,' ke cabang ',ip_tujuan,' gagal.')
+			return
+	try:
+		body_post_transfer = {'user_id':user_id, 'nilai':jumlah_transfer}
+		resp_transfer = requests.post('http://'+ip_tujuan+'/ewallet/transfer', json = body_post_transfer)
+		body_transfer_unicode = resp_transfer.text
+		body_transfer = json.loads(body_transfer_unicode)
+		if str(body_transfer['status_transfer']) == '1':
+			nasabah.saldo = nasabah.saldo - jumlah_transfer
+			nasabah.save()
+			print('Transfer sejumlah ',jumlah_transfer,' ke cabang ',ip_tujuan,' berhasil.')
+		else:
+			print('Transfer sejumlah ',jumlah_transfer,' ke cabang ',ip_tujuan,' gagal.')
+	except:
+		print('Terjadi kesalahan. Transfer sejumlah ',jumlah_transfer,' ke cabang ',ip_tujuan,' gagal.')
